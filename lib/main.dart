49ee0b167a09bf675e5b9e91f17db8b99b6eb962
AgentGalon <zahranrafif911@gmail.com>
2023-10-22 23:09:38 +0700

    update with provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
 
 import 'package:car_wash_app/screens/home_screen.dart';
 import 'package:car_wash_app/screens/login_screen.dart';
+import 'package:car_wash_app/providers/login_provider.dart';
+import 'package:car_wash_app/providers/home_screen_provider.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  bool isLoggedIn = false;
-
-  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.red,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => AuthProvider()),
+        ChangeNotifierProvider(create: (context) => HomeScreenModel()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.red,
+          textTheme: GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme),
+        ),
+        home: Consumer<AuthProvider>(
+          builder: (context, authProvider, child) {
+            return authProvider.isLoggedIn
+                ? const HomeScreen()
+                : const LoginPage();
+          },
+        ),
       ),
-      home: isLoggedIn ? const HomeScreen() : const LoginPage(),
     );
   }
 }
