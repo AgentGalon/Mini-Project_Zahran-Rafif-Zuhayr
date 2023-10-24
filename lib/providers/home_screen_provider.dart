e4d7b263bdd826e31fa58680f1fd1bec3be99b3d
AgentGalon <zahranrafif911@gmail.com>
2023-10-25 00:22:50 +0700

    add some more logic

--- lib/providers/home_screen_provider.dart
+++ lib/providers/home_screen_provider.dart
@@ -11,9 +11,11 @@ enum Category {
 class HomeScreenModel extends ChangeNotifier {
   int _currentIndex = 0;
   Category _selectedCategory = Category.all;
+  final List<Map<String, dynamic>> _favoriteServices = [];
 
   int get currentIndex => _currentIndex;
   Category get selectedCategory => _selectedCategory;
+  List<Map<String, dynamic>> get favoriteServices => _favoriteServices;
 
   void setTabIndex(int index) {
     _currentIndex = index;
@@ -22,6 +24,18 @@ class HomeScreenModel extends ChangeNotifier {
 
   void setSelectedCategory(Category category) {
     _selectedCategory = category;
+    notifyListeners();
+  }
+
+  void addToFavorites(Map<String, dynamic> service) {
+    if (!_favoriteServices.contains(service)) {
+      _favoriteServices.add(service);
+      notifyListeners();
+    }
+  }
+
+  void removeFromFavorites(Map<String, dynamic> service) {
+    _favoriteServices.remove(service);
     notifyListeners();
   }
 }
