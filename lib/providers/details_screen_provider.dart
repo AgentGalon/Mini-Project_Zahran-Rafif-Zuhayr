ba9d34bd080dd5f1f653dfb2f8fc4a71410beb3b
AgentGalon <zahranrafif911@gmail.com>
2023-10-25 00:22:13 +0700

    delete unused code

--- lib/providers/details_screen_provider.dart
+++ lib/providers/details_screen_provider.dart
@@ -1,22 +1,6 @@
 import 'package:flutter/foundation.dart';
 
 class DetailScreenProvider with ChangeNotifier {
-  // double _totalPrice = 0.0;
-  // List<bool> _additionalServices = List.generate(7, (index) => false);
-
-  // double get totalPrice => _totalPrice;
-  // List<bool> get additionalServices => _additionalServices;
-
-  // void toggleAdditionalService(int index, double price) {
-  //   _additionalServices[index] = !_additionalServices[index];
-  //   if (_additionalServices[index]) {
-  //     _totalPrice += price;
-  //   } else {
-  //     _totalPrice -= price;
-  //   }
-  //   notifyListeners();
-  // }
-
   bool _isFavorite = false;
 
   bool get isFavorite => _isFavorite;
