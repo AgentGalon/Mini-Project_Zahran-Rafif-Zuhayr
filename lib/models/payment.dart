291590a127a982d6d5bdf5d1381d36095f7b5697
AgentGalon <zahranrafif911@gmail.com>
2023-10-26 00:04:41 +0700

    add class for payment

---
+++ lib/models/payment.dart
@@ -0,0 +1,11 @@
+class Payment {
+  final String selectedPackage;
+  final String selectedCarType;
+  final double totalPrice;
+
+  Payment({
+    required this.selectedPackage,
+    required this.selectedCarType,
+    required this.totalPrice,
+  });
+}
