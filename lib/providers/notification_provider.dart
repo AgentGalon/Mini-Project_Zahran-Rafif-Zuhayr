d4bd0095e7cbca2bd4ce92bcefa65c8191e0bca2
AgentGalon <zahranrafif911@gmail.com>
2023-10-30 14:57:50 +0700

    add storage in notification

--- lib/providers/notification_provider.dart
+++ lib/providers/notification_provider.dart
@@ -1,10 +1,42 @@
 import 'package:flutter/foundation.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class NotificationProvider extends ChangeNotifier {
   List<String> notifications = [];
 
-  void addNotification(String notification) {
+  NotificationProvider() {
+    // Mengambil notifikasi yang tersimpan dalam Shared Preferences saat aplikasi dimuat
+    loadNotifications();
+  }
+
+  Future<void> addNotification(String notification) async {
     notifications.add(notification);
     notifyListeners();
+
+    // Menyimpan notifikasi yang baru ke Shared Preferences
+    await saveNotifications();
+  }
+
+  Future<void> loadNotifications() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    List<String> savedNotifications =
+        prefs.getStringList('notifications') ?? [];
+    notifications.addAll(savedNotifications);
+    notifyListeners();
+  }
+
+  Future<void> saveNotifications() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setStringList('notifications', notifications);
+  }
+
+  void clearNotifications() {
+    notifications.clear();
+    notifyListeners();
+  }
+
+  Future<void> clearSavedNotifications() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.remove('notifications');
   }
 }
--- lib/screens/notification_screen.dart
+++ lib/screens/notification_screen.dart
@@ -4,30 +4,55 @@ import 'package:provider/provider.dart';
 import 'package:car_wash_app/providers/notification_provider.dart';
 
 class NotificationScreen extends StatelessWidget {
-  const NotificationScreen({super.key});
+  const NotificationScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final notifications =
-        Provider.of<NotificationProvider>(context, listen: false).notifications;
+    final notificationProvider = Provider.of<NotificationProvider>(context);
 
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        foregroundColor: Colors.black,
         elevation: 0,
         centerTitle: true,
         title: const Text("Notification"),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.delete),
+            onPressed: () {
+              // Hapus notifikasi dari NotificationProvider
+              notificationProvider.clearNotifications();
+
+              // Hapus notifikasi dari shared_preferences
+              notificationProvider.clearSavedNotifications();
+            },
+          ),
+        ],
       ),
-      body: ListView.builder(
-        itemCount: notifications.length,
-        itemBuilder: (context, index) {
-          final notification = notifications[index];
-          return ListTile(
-            leading: const Icon(Icons.notifications),
-            title: const Text(
-              "Notification",
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-            subtitle: Text(notification),
+      body: Consumer<NotificationProvider>(
+        builder: (context, provider, child) {
+          final notifications = provider.notifications;
+
+          if (notifications.isEmpty) {
+            return const Center(
+              child: Text("No notifications"),
+            );
+          }
+
+          return ListView.builder(
+            itemCount: notifications.length,
+            itemBuilder: (context, index) {
+              final notification = notifications[index];
+              return ListTile(
+                leading: const Icon(Icons.notifications),
+                title: const Text(
+                  "Notification",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                subtitle: Text(notification),
+              );
+            },
           );
         },
       ),
